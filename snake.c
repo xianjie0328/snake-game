a2d6bf3e4f475ac8c247a153861ee3f7409dc282
xianjie0328 <xianjie0328@users.noreply.github.com>
2026-06-15 00:25:25 +0800

    Add simple snake game in C

---
+++ snake.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+#define WIDTH 20
+#define HEIGHT 20
+
+int x, y, foodX, foodY, score, gameOver;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eDirection dir;
+
+void Setup() {
+    gameOver = 0;
+    dir = STOP;
+    x = WIDTH / 2;
+    y = HEIGHT / 2;
+    foodX = rand() % WIDTH;
+    foodY = rand() % HEIGHT;
+    score = 0;
+    nTail = 0;
+}
+
+void Draw() {
+    system("cls");
+    printf("Score: %d\n", score);
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
+    printf("\n");
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (j == 0)
+                printf("#");
+            if (i == y && j == x)
+                printf("O");
+            else if (i == foodY && j == foodX)
+                printf("*");
+            else {
+                int print = 0;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        printf("o");
+                        print = 1;
+                    }
+                }
+                if (!print)
+                    printf(" ");
+            }
+            if (j == WIDTH - 1)
+                printf("#");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
+    printf("\n");
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a':
+            if (dir != RIGHT) dir = LEFT;
+            break;
+        case 'd':
+            if (dir != LEFT) dir = RIGHT;
+            break;
+        case 'w':
+            if (dir != DOWN) dir = UP;
+            break;
+        case 's':
+            if (dir != UP) dir = DOWN;
+            break;
+        case 'x':
+            gameOver = 1;
+            break;
+        }
+    }
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir) {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    if (x >= WIDTH) x = 0; else if (x < 0) x = WIDTH - 1;
+    if (y >= HEIGHT) y = 0; else if (y < 0) y = HEIGHT - 1;
+
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = 1;
+    }
+
+    if (x == foodX && y == foodY) {
+        score += 10;
+        foodX = rand() % WIDTH;
+        foodY = rand() % HEIGHT;
+        nTail++;
+    }
+}
+
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        Sleep(100);
+    }
+    printf("Game Over! Final Score: %d\n", score);
+    return 0;
+}
